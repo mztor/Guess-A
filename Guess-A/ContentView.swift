f2b2118c65cd71b0ec6689ae5c84a57856f22bda
mztor <stor@macarthur.nsw.edu.au>
2022-06-23 09:26:27 +1000

    Initial Commit

---
+++ Guess-A/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess-A
+//
+//  Created by Sarah Tor on 23/6/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guess-A/Guess_AApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_AApp.swift
+//  Guess-A
+//
+//  Created by Sarah Tor on 23/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct Guess_AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

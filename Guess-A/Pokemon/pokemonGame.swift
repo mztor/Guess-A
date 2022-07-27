c678d6f0cc545b1aede8fa10ff02eadc0b43a0ac
whyyyyyyyyyyyyyyyyyyyyyy <milnew@macstudent.com.au>
2022-07-28 09:08:39 +1000

    Navigation links working for levels of easy and hard

---
+++ Guess-A/Pokemon/pokemonGame.swift
@@ -0,0 +1,20 @@
+//
+//  pokemonGame.swift
+//  Guess-A
+//
+//  Created by William Milne on 28/7/2022.
+//
+
+import SwiftUI
+
+struct pokemonGame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct pokemonGame_Previews: PreviewProvider {
+    static var previews: some View {
+        pokemonGame()
+    }
+}
--- Guess-A/Pokemon/pokemonLevels.swift
+++ Guess-A/Pokemon/pokemonLevels.swift
@@ -10,12 +10,26 @@ import SwiftUI
 struct pokemonLevels: View {
     var body: some View {
         VStack {
-            Text("GUESS")
-                .padding(0.2)
-            Text("A")
-                .padding(0.2)
-            Text("POKÉMON")
-                .padding(0.2)
+            VStack {
+                Text("GUESS")
+                    .padding(0.2)
+                Text("A")
+                    .padding(0.2)
+                Text("POKÉMON")
+                    .padding(0.2)
+            }
+            .padding()
+            Text("Difficulty Selection")
+                .padding()
+            VStack {
+                NavigationLink(destination: pokemonGame()) {
+                    Text("Easy")
+                }
+                .padding()
+                NavigationLink(destination: pokemonGame()) {
+                    Text("Hard")
+                }
+            }
         }
     }
 }

2c86da218c9efae1208849d889c62acaca072b6c
whyyyyyyyyyyyyyyyyyyyyyy <milnew@macstudent.com.au>
2022-08-05 21:22:12 +1000

    Random numbers now call for specific Pokemon however a logic error has been discovered and I have no idea how to fix it because every other thing I try is some sort of ridiculous syntax error, this particular error won't have a negative impact on gameplay assuming that the user enjoys clicking out of the game and clicking again on the same difficulty they chose first every time they open up the game. Also expansion on Pokédex

--- Guess-A/Pokemon/pokemonGame.swift
+++ Guess-A/Pokemon/pokemonGame.swift
@@ -7,30 +7,53 @@
 
 import SwiftUI
 
+
+
 var randomNum = 0
-var msg = ""
+var pokemon = ""
 struct pokemonGame: View {
     var level: String
     var body: some View {
-        Text(msg)
+        Text("Guess a pokémon")
         .onAppear() {
-            randomNum = Int.random(in: 1...5)
-            if randomNum == 1 {
-                msg = "1"
+            if level == "easy" {
+                randomNum = Int.random(in: 1...5)
+                if randomNum == 1 {
+                    pokemon = numbered.pkm1
+                }
+                if randomNum == 2 {
+                    pokemon = numbered.pkm2
+                }
+                if randomNum == 3 {
+                    pokemon = numbered.pkm3
+                }
+                if randomNum == 4 {
+                    pokemon = numbered.pkm4
+                }
+                if randomNum == 5 {
+                    pokemon = numbered.pkm5
+                }
             }
-            if randomNum == 2 {
-                msg = "2"
-            }
-            if randomNum == 3 {
-                msg = "3"
-            }
-            if randomNum == 4 {
-                msg = "4"
-            }
-            if randomNum == 5 {
-                msg = "5"
+            if level == "hard" {
+                randomNum = Int.random(in: 6...10)
+                if randomNum == 6 {
+                    pokemon = numbered.pkm6
+                }
+                if randomNum == 7 {
+                    pokemon = numbered.pkm7
+                }
+                if randomNum == 8 {
+                    pokemon = numbered.pkm8
+                }
+                if randomNum == 9 {
+                    pokemon = numbered.pkm9
+                }
+                if randomNum == 10 {
+                    pokemon = numbered.pkm10
+                }
             }
         }
+        Text(pokemon)
     }
 }
 
--- Guess-A/Pokemon/pokemonPokedex.swift
+++ Guess-A/Pokemon/pokemonPokedex.swift
@@ -11,11 +11,27 @@ struct pokedex {
     let pkm1: String
     let pkm2: String
     let pkm3: String
+    let pkm4: String
+    let pkm5: String
+    let pkm6: String
+    let pkm7: String
+    let pkm8: String
+    let pkm9: String
+    let pkm10: String
+    let pkm11: String
 }
 
 let numbered = pokedex(
     pkm1: "Bulbasaur",
     pkm2: "Ivysaur",
-    pkm3: "Venasaur"
+    pkm3: "Venasaur",
+    pkm4: "Charmander",
+    pkm5: "Charmeleon",
+    pkm6: "Charizard",
+    pkm7: "Squirtle",
+    pkm8: "Wartortle",
+    pkm9: "Blastoise",
+    pkm10: "Caterpie",
+    pkm11: "Metapod"
 )
 

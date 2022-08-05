1e037c21a9f16a786167ea70d93d7a194e68d485
whyyyyyyyyyyyyyyyyyyyyyy <milnew@macstudent.com.au>
2022-08-05 20:37:35 +1000

    Random number generator finally works after a lot of errors that make no sense, they made me want to throw the computer out a window and seriously made me live up to my Github name

--- Guess-A/Pokemon/pokemonGame.swift
+++ Guess-A/Pokemon/pokemonGame.swift
@@ -7,12 +7,29 @@
 
 import SwiftUI
 
+var randomNum = 0
+var msg = ""
 struct pokemonGame: View {
     var level: String
-    
     var body: some View {
-        for _ in 1...3 {
-            Text(Int.random(in: 1...5))
+        Text(msg)
+        .onAppear() {
+            randomNum = Int.random(in: 1...5)
+            if randomNum == 1 {
+                msg = "1"
+            }
+            if randomNum == 2 {
+                msg = "2"
+            }
+            if randomNum == 3 {
+                msg = "3"
+            }
+            if randomNum == 4 {
+                msg = "4"
+            }
+            if randomNum == 5 {
+                msg = "5"
+            }
         }
     }
 }

09e3b4b68c83044296d072cec5796f6df39e2a14
whyyyyyyyyyyyyyyyyyyyyyy <milnew@macstudent.com.au>
2022-08-06 18:46:21 +1000

    Found a massive time and space saving solution to the storage of Pokemon names that makes me feel like an idiot for not realising earlier and even using that old method 51 times! like that was so much effort to do even with copy and paste

--- Guess-A/Pokemon/pokemonPokedex.swift
+++ Guess-A/Pokemon/pokemonPokedex.swift
@@ -7,112 +7,12 @@
 
 import Foundation
 
-struct pokedex {
-    let pkm1: String
-    let pkm2: String
-    let pkm3: String
-    let pkm4: String
-    let pkm5: String
-    let pkm6: String
-    let pkm7: String
-    let pkm8: String
-    let pkm9: String
-    let pkm10: String
-    let pkm11: String
-    let pkm12: String
-    let pkm13: String
-    let pkm14: String
-    let pkm15: String
-    let pkm16: String
-    let pkm17: String
-    let pkm18: String
-    let pkm19: String
-    let pkm20: String
-    let pkm21: String
-    let pkm22: String
-    let pkm23: String
-    let pkm24: String
-    let pkm25: String
-    let pkm26: String
-    let pkm27: String
-    let pkm28: String
-    let pkm29: String
-    let pkm30: String
-    let pkm31: String
-    let pkm32: String
-    let pkm33: String
-    let pkm34: String
-    let pkm35: String
-    let pkm36: String
-    let pkm37: String
-    let pkm38: String
-    let pkm39: String
-    let pkm40: String
-    let pkm41: String
-    let pkm42: String
-    let pkm43: String
-    let pkm44: String
-    let pkm45: String
-    let pkm46: String
-    let pkm47: String
-    let pkm48: String
-    let pkm49: String
-    let pkm50: String
-    let pkm51: String
+struct pokemonList {
+    let pokedex: [String]
 }
 
-let numbered = pokedex(
-    pkm1: "Bulbasaur",
-    pkm2: "Ivysaur",
-    pkm3: "Venusaur",
-    pkm4: "Charmander",
-    pkm5: "Charmeleon",
-    pkm6: "Charizard",
-    pkm7: "Squirtle",
-    pkm8: "Wartortle",
-    pkm9: "Blastoise",
-    pkm10: "Caterpie",
-    pkm11: "Metapod",
-    pkm12: "Butterfree",
-    pkm13: "Weedle",
-    pkm14: "Kakuna",
-    pkm15: "Beedrill",
-    pkm16: "Pidgey",
-    pkm17: "Pidgeotto",
-    pkm18: "Pidgeot",
-    pkm19: "Rattata",
-    pkm20: "Raticate",
-    pkm21: "Spearow",
-    pkm22: "Fearow",
-    pkm23: "Ekans",
-    pkm24: "Arbok",
-    pkm25: "Pikachu",
-    pkm26: "Raichu",
-    pkm27: "Sandshrew",
-    pkm28: "Sandslash",
-    pkm29: "Nidoran♀",
-    pkm30: "Nidorina",
-    pkm31: "Nidoqueen",
-    pkm32: "Nidoran♂",
-    pkm33: "Nidorino",
-    pkm34: "Nidoking",
-    pkm35: "Clefairy",
-    pkm36: "Clefable",
-    pkm37: "Vulpix",
-    pkm38: "Ninetales",
-    pkm39: "Jigglypuff",
-    pkm40: "Wigglytuff",
-    pkm41: "Zubat",
-    pkm42: "Golbat",
-    pkm43: "Oddish",
-    pkm44: "Gloom",
-    pkm45: "Vileplume",
-    pkm46: "Paras",
-    pkm47: "Parasect",
-    pkm48: "Venonat",
-    pkm49: "Venomoth",
-    pkm50: "Diglett",
-    pkm51: "Dugtrio"
+let numbered = pokemonList(
+    pokedex: ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow","Fearow","Ekans", "Arbok", "Pikachu", "Raichu","Sandshrew", "Sandslash", "Nidoran♀", "Nidorina", "Nidoqueen", "Nidoran♂", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio"]
     
 )
 

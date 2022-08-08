802d50b032f247605b64fcba5d511e423b85d70d
harrisa0 <awanh@macstudent.com.au>
2022-08-08 14:30:36 +1000

    Game difficulty selection creation

--- Guess-A/NBA team/nbaTeamLevels.swift
+++ Guess-A/NBA team/nbaTeamLevels.swift
@@ -9,12 +9,47 @@ import SwiftUI
 
 struct nbaTeamLevels: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            VStack{Text("Guess")
+                    .font(.largeTitle)
+                    .padding(0.2)
+                Text("a")
+                    .font(.largeTitle)
+                    .padding(0.2)
+                Text("NBA Team")
+                    .font(.largeTitle)
+                    .padding(0.2)
+            }
+        VStack{
+            Image("bball")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 150)
+        }
+            VStack{
+                Text("Difficulty Selection")
+                
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+        .padding()
     }
 }
 
 struct nbaTeam_Previews: PreviewProvider {
     static var previews: some View {
         nbaTeamLevels()
+            .frame(width: 0.0, height: 0.0)
     }
 }

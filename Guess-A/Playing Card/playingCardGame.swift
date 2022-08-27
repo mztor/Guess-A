154bb6d4dbcf017d742a5e4b85147918c8fabf55
Jobraatzel <braatzj@macstudent.com.au>
2022-08-27 22:35:07 +1000

    Added Significant White Space

--- Guess-A/Playing Card/playingCardGame.swift
+++ Guess-A/Playing Card/playingCardGame.swift
@@ -8,148 +8,244 @@
 import SwiftUI
 
 struct playingCardGame: View {
-    var level: String //variable declaration
+    
+    //variable declaration
+    
+    var level: String
+    
     var correctOrNot = ""
+    
     @State var showValue = "?"
+    
     @State var suiteGuess = ""
+    
     @State var valueGuess = ""
+    
     let suitesArray = ["♠️", "♦️", "♣️", "♥️"]
+    
     let valuesArray = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    
     @State var randomSuiteNum = Int.random(in: 0...3)
+    
     @State var randomValueNum = Int.random(in: 0...12)
+    
     @State var cardColour = ""
+    
+    @State var guessCounter = 0
+    
     @State private var showHelpCard = false
+    
     @State private var showHintCard = false
+    
     func checkGuesses() {
-//        if suiteGuess == suitesArray(randomSuiteNum) {
-//
-//        }
+        
+        if suiteGuess == suitesArray[randomSuiteNum] {
+            
+        }
+    }
+    func setColour() { //set colour of card for hint on Beginner difficult
+        
+        if suitesArray[randomSuiteNum] == "♠️" || suitesArray[randomSuiteNum] == "♣️" {
+            
+            cardColour = "black"
+        }
+        
+        if suitesArray[randomSuiteNum] == "♦️" || suitesArray[randomSuiteNum] == "♥️" {
+            
+            cardColour = "red"
+        }
     }
     var body: some View {
+        
         VStack{
+            
             HStack{
+                
                 Text("?")
                     .foregroundColor(.white)
                     .font(.largeTitle)
                     .frame(width: 75.0, height: 115.0)
                     .background(RoundedRectangle(cornerRadius: 10).fill(Color.black))
+                
                 VStack{
+                    
                     HStack {
                         Text("♠️")
                             .font(.largeTitle)
+                        
                         Text("♦️")
                             .font(.largeTitle)
+                        
                         Text("♣️")
                             .font(.largeTitle)
+                        
                         Text("♥️")
                             .font(.largeTitle)
+                        
                     }
+                    
                     Text("Number: " + showValue)
                         .font(.title)
                 }
             }
-            VStack{ //This VSTack starts all the code for the buttons
+            VStack{ //This VStack starts all the code for the buttons
+               
                 HStack{
+                    
                     Button {
+                        
                         suiteGuess = "♠️"
+                        
                     } label: {
+                        
                         Text("♠️")
                             .font(.largeTitle)
+                        
                     }
                     Button {
+                        
                         suiteGuess = "♦️"
+                        
                     } label: {
+                        
                         Text("♦️")
                             .font(.largeTitle)
                     }
                     Button {
+                        
                         suiteGuess = "♣️"
+                        
                     } label: {
+                        
                         Text("♣️")
                             .font(.largeTitle)
                     }
                     Button {
+                        
                         suiteGuess = "♥️"
+                        
                     } label: {
+                        
                         Text("♥️")
                             .font(.largeTitle)
                     }
                 }
                 .buttonStyle(.bordered)
+                
                 HStack {
+                    
                     Button {
+                        
                         valueGuess = "A"
+                        
                     } label: {
+                        
                         Text("A")
                             .font(.largeTitle)
                             .foregroundColor(.black)
                     }
                     .padding(.horizontal)
+                    
                     Button {
+                        
                         valueGuess = "2"
+                        
                     } label: {
+                        
                         Text("2")
                             .font(.largeTitle)
                             .foregroundColor(.black)
                     }
                     .padding(.horizontal)
+                    
                     Button {
+                        
                         valueGuess = "3"
+                        
                     } label: {
+                        
                         Text("3")
                             .font(.largeTitle)
                             .foregroundColor(.black)
                     }
                     .padding(.horizontal)
+                    
                 }
                 .buttonStyle(.bordered)
+                
                 HStack {
+                    
                     Button {
+                        
                         valueGuess = "4"
+                        
                     } label: {
+                        
                         Text("4")
                             .font(.largeTitle)
                             .foregroundColor(.black)
                     }
                     .padding(.horizontal)
+                    
                     Button {
+                        
                         valueGuess = "5"
+                        
                     } label: {
+                        
                         Text("5")
                             .font(.largeTitle)
                             .foregroundColor(.black)
                     }
                     .padding(.horizontal)
+                    
                     Button {
+                        
                         valueGuess = "6"
+                        
                     } label: {
+                        
                         Text("6")
                             .font(.largeTitle)
                             .foregroundColor(.black)
                     }
                     .padding(.horizontal)
+                    
                 }
                 .buttonStyle(.bordered)
+                
                 HStack {
+                    
                     Button {
+                        
                         valueGuess = "7"
+                        
                     } label: {
+                        
                         Text("7")
                             .font(.largeTitle)
                             .foregroundColor(.black)
                     }
                     .padding(.horizontal)
+                    
                     Button {
+                        
                         valueGuess = "8"
+                        
                     } label: {
+                        
                         Text("8")
                             .font(.largeTitle)
                             .foregroundColor(.black)
                     }
                     .padding(.horizontal)
+                    
                     Button {
+                        
                         valueGuess = "9"
+                        
                     } label: {
+                        
                         Text("9")
                             .font(.largeTitle)
                             .foregroundColor(.black)
@@ -157,73 +253,110 @@ struct playingCardGame: View {
                     .padding(.horizontal)
                 }
                 .buttonStyle(.bordered)
+                
                 HStack {
+                    
                     Button {
+                        
                         valueGuess = "10"
+                        
                     } label: {
+                        
                         Text("10")
                             .font(.largeTitle)
                             .foregroundColor(.black)
                     }
                     .padding(.leading)
+                    
                     Button {
+                        
                         valueGuess = "J"
+                        
                     } label: {
+                        
                         Text("J")
                             .font(.largeTitle)
                             .foregroundColor(.black)
                     }
                     .padding(.horizontal)
+                    
                     Button {
+                        
                         valueGuess = "Q"
+                        
                     } label: {
+                        
                         Text("Q")
                             .font(.largeTitle)
                             .foregroundColor(.black)
                     }
                     .padding(.horizontal)
+                    
                 }
                 .buttonStyle(.bordered)
+                
                 HStack {
+                    
                     Button {
+                        
                         valueGuess = "K"
+                        
                     } label: {
+                        
                         Text("K")
                             .font(.largeTitle)
                             .foregroundColor(.black)
                     }
                     .padding(.horizontal)
+                    
                     Button {
                         
                     } label: {
+                        
                         Text("ENTER")
                             .font(.largeTitle)
                             .foregroundColor(.green)
                     }
                     .padding(.horizontal)
+                    
                 }
                 .buttonStyle(.bordered)
+                
             }
             Spacer()
+            
         }
         .toolbar {
+            
             HStack {
+                
             if level == "Beginner" {
+                
                 Button() {
+                    
                     showHintCard.toggle()
+                    
                 } label: {
+                    
                     Image(systemName: "lightbulb.circle")
+                    
                 }
                 .alert(isPresented: $showHintCard) {
+                    
                     Alert(title: Text("Hint!"), message: Text("The cards color is " + cardColour), dismissButton: .default(Text("Ok")))
                 }
             }
             Button() {
+                
                 showHelpCard.toggle()
+                
             } label: {
+                
                 Image(systemName: "questionmark.circle")
+                
             }
             .alert(isPresented: $showHelpCard) {
+                
                 Alert(title: Text("Help!"), message: Text("Select one of the Suite buttons and a value for your card, then press Enter!"), dismissButton: .default(Text("Ok")))
             }
             }
--- Guess-A/Playing Card/playingCardLevels.swift
+++ Guess-A/Playing Card/playingCardLevels.swift
@@ -8,37 +8,50 @@
 import SwiftUI
 
 struct playingCardLevels: View {
+    
     @State private var showHelpCard = false
+    
     var body: some View {
+        
         VStack {
+            
             VStack {
+                
                 Text("GUESS")
                     .font(.largeTitle)
                     .fontWeight(.black)
                     .padding(0.2)
+                
                 Text("A")
                     .font(.largeTitle)
                     .fontWeight(.black)
                     .padding(0.2)
+                
                 Text("PL♠️YING C♦️RD")
                     .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
                     .fontWeight(.black)
                     .padding(0.2)
             }
             .padding()
+            
             Spacer()
+            
             VStack {
                 Spacer()
+                
                 Text("Choose Your Difficulty!")
                     .font(.title2)
                     .fontWeight(.black)
                     .foregroundColor(Color(hue: 0.0, saturation: 0.844, brightness: 0.512))
                     .multilineTextAlignment(.center)
                     .padding(0.2)
+                
                 RoundedRectangle(cornerRadius: 100, style: .continuous)
                     .frame(width: 240, height: 3)
                     .padding(0.4)
+                
                 Spacer()
+                
                 NavigationLink(destination: playingCardGame(level: "Beginner")) {
                     Text("Beginner")
                         .font(.title2)
@@ -46,6 +59,7 @@ struct playingCardLevels: View {
                         .foregroundColor(.green)
                         .background(RoundedRectangle(cornerRadius: 18).fill(Color.black).frame(width: 165, height: 50))
                         .padding()
+                    
                 }
                 NavigationLink(destination: playingCardGame(level: "Gambler")) {
                     Text("Gambler")
@@ -54,6 +68,7 @@ struct playingCardLevels: View {
                         .foregroundColor(.yellow)
                         .background(RoundedRectangle(cornerRadius: 18).fill(Color.black).frame(width: 165, height: 50))
                         .padding()
+                    
                 }
                 NavigationLink(destination: playingCardGame(level: "Psychic")) {
                     Text("Psychic")
@@ -62,17 +77,26 @@ struct playingCardLevels: View {
                         .foregroundColor(.red)
                         .background(RoundedRectangle(cornerRadius: 18).fill(Color.black).frame(width: 165, height: 50))
                         .padding()
+                    
                 }
+                
                 Spacer()
+                
             }
         }
         .toolbar {
+            
             Button() {
+                
                 showHelpCard.toggle()
+            
             } label: {
+                
                 Image(systemName: "questionmark.circle")
+                
             }
             .alert(isPresented: $showHelpCard) {
+                
                 Alert(title: Text("Help!"), message: Text("Select one of the difficulty buttons below to start the game!"), dismissButton: .default(Text("Ok")))
             }
         }
@@ -80,7 +104,9 @@ struct playingCardLevels: View {
 }
 
 struct playingCardLevels_Previews: PreviewProvider {
+    
     static var previews: some View {
+        
         playingCardLevels()
     }
 }

ce5ab9236a7f4e7d01947e4013f89714fd1fbf74
AlexandraSouthern <southerna@macstudent.com.au>
2022-09-15 10:48:16 +1000

    Images for Intermediate level
    Added images  for intermediate level of all the marvel characters

--- Guess-A/Marvel Character/ marvelLevels.swift
+++ Guess-A/Marvel Character/ marvelLevels.swift
@@ -17,36 +17,43 @@ struct marvelLevels: View {
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .padding(0.2)
+                    .multilineTextAlignment(.center)
+
                 Text("- A -")
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .padding(0.2)
+                    .multilineTextAlignment(.center)
+
                 Text("MARVEL CHARACTER")
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .padding(0.2)
                     .foregroundColor(.red)
+                    .multilineTextAlignment(.center)
+
             }
             .padding()
+       
             VStack {
                 
                 VStack {
-                    NavigationLink(destination: marvelGame()) {
+                
                         Text("Beginner")
                             .foregroundColor(.black)
                             .fontWeight(.bold)
                             .background(RoundedRectangle(cornerRadius: 18).fill(Color.red).frame(width: 165, height: 50))
                     }
-                    .padding(20)
+                    .padding(1)
                     
-                    NavigationLink(destination: marvelGame()) {
+                    NavigationLink(destination: marvelGame(level: "Intermediate")) {
                         Text("Intermediate")
                             .foregroundColor(.black)
                             .fontWeight(.bold)
                             .background(RoundedRectangle(cornerRadius: 18).fill(Color.red).frame(width: 165, height: 50))
                     }
-                    .padding(20)
-                    NavigationLink(destination: marvelGame()) {
+                    .padding(40)
+                   
                         Text("Advanced")
                             .foregroundColor(.black)
                             .fontWeight(.bold)
@@ -56,18 +63,7 @@ struct marvelLevels: View {
                 }
                 .padding(25)
             }
-            .padding(.vertical, 80)
-            }
-       
-        .toolbar {
-            Button("Help"){
-                showHelp()
-            }
-       
         }
-   
-    }
-}
 struct marvelLevels_Previews: PreviewProvider {
     static var previews: some View {
         marvelLevels()
--- Guess-A/Marvel Character/MarvelGame.swift
+++ Guess-A/Marvel Character/MarvelGame.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct marvelGame: View {
+    
+    var level: String
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+        VStack {
+            VStack {
+                Text("GUESS")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding(0.2)
+               
+                Text("- A -")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding(0.2)
+               
+                Text("MARVEL CHARACTER")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding(0.2)
+                    .foregroundColor(.red)
+            }
+            .padding()
+            Spacer()
+
+        }
+        .toolbar {
+            Button("Help"){
+                showHelp()
+                            }
+        }
+
     }
 }
-
 struct marvelGame_Previews: PreviewProvider {
     static var previews: some View {
-        marvelGame()
+        marvelGame(level: "")
+        }
     }
-}
+
+

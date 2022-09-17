1e1e07d192eda846e2173a8482bc27d8d785e835
Ben <abdyb@macstudent.com.au>
2022-09-17 12:35:38 +1000

    Finished carBrandLevels screen and made working difficulty buttons which go to carBrandGame screen

--- Guess-A/Car Brand/carBrandLevels.swift
+++ Guess-A/Car Brand/carBrandLevels.swift
@@ -7,9 +7,52 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct carBrandLevels: View {
     var body: some View {
-        Text("Hello, Ben was here!")
+        VStack {
+//            RoundedRectangle(cornerRadius: 100, style: .continuous)
+//                .frame(width: 414, height: 1)
+//                .padding(.bottom, 60)
+            VStack {
+                Text("GUESS THE CAR BRAND")
+                    .font(.title)
+                    .padding(20)
+//                    .padding(.top, 40)
+            }
+                VStack {
+                    Text("Choose A")
+                        .font(.largeTitle)
+                    
+                    Text("Difficulty")
+                        .font(.largeTitle)
+                        .padding(.bottom, 60.0)
+                    
+                    VStack {
+                        NavigationLink(destination: carBrandGame()) {
+                            Text("EASY")
+                                .fontWeight(.bold)
+                                .foregroundColor(.black)
+                                .padding()
+                                .font(.title2)
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.green).frame(width: 200, height: 150))
+                        }
+                        .padding(50)
+                        NavigationLink(destination: carBrandGame()) {
+                            Text("HARD")
+                                .font(.title2)
+                                .fontWeight(.bold)
+                                .padding()
+                                .foregroundColor(.black)
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.red).frame(width: 200, height: 150))
+                        }
+                        .padding(50)
+                        .padding(.top, 20)
+                    }
+                }
+            }
+        .padding()
     }
 }
 
@@ -18,3 +61,4 @@ struct carBrandLevels_Previews: PreviewProvider {
         carBrandLevels()
     }
 }
+

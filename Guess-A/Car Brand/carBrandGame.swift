69cb33fd4e7c19fa4e100de4e16a35dadc466bf8
Ben <abdyb@macstudent.com.au>
2022-09-17 12:43:15 +1000

    Finished carBrandLevels screen and made working difficulty buttons which go to carBrandGame screen

---
+++ Guess-A/Car Brand/carBrandGame.swift
@@ -0,0 +1,20 @@
+//
+//  carBrandGame.swift
+//  Guess-A
+//
+//  Created by Benjamin Abdy on 17/9/2022.
+//
+
+import SwiftUI
+
+struct carBrandGame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct carBrandGame_Previews: PreviewProvider {
+    static var previews: some View {
+        carBrandGame()
+    }
+}
